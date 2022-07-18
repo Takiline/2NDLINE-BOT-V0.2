d97cf7ecd835c63812d2cfec14de7c5da31ce777
Takiline <109474093+Takiline@users.noreply.github.com>
2022-07-18 14:23:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919832518780'] //ur owner number
+global.ownername = "🤩 TAKIKUN 😍" //ur owner name
+global.ytname = "https://youtube.com/channel/UCfuyuz1lDd9GGnR0dcc4o9A" //ur yt chanel name
+global.socialm = "https://github.com/Takiline" //ur github or insta name
+global.location = "India, West Bangal, Siliguri" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = ['919832518780']
+global.ownertag = '919832518780' //ur tag number
+global.botname = 'TAKIBOT V0.2' //ur bot name
+global.linkz = "https://youtube.com/channel/UCfuyuz1lDd9GGnR0dcc4o9A" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = 'https://github.com/Takiline' //script link
+global.reactmoji = "🤗" //ur menu react emoji
+global.themeemoji = "🤖" //ur theme emoji
+global.packname = "😊TAKIKUN +919832518780😊Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "😊TAKIKUN +919832518780😊" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
